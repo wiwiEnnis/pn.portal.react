22afe99b6c4c86d9ed4f200878cfbafeda0840db
wiwiEnnis <ennis.lu@pentium.network>
2020-02-21 11:07:05 +0800

    Updates

---
+++ precache-manifest.67873a4cd1f386d4ff8d60805c3c660c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e8ecd957d5db88af77a9cd7a297a99a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb4e037005051d94fed9",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1561932a1456dd31ab4b",
+    "url": "/static/js/2.c0b65204.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.c0b65204.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb4e037005051d94fed9",
+    "url": "/static/js/main.40377206.chunk.js"
+  },
+  {
+    "revision": "31f77ba02dc2215d28b1",
+    "url": "/static/js/runtime-main.b26177c9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
